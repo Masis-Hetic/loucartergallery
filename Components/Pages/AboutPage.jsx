d10ff2ae6dc4c9747e65fdc855753182542b7222
CedricSalaun <salaun.cedric@gmail.com>
2019-05-16 01:12:00 +0200

    add fist data display from prismic into about page

--- Components/Pages/AboutPage.jsx
+++ Components/Pages/AboutPage.jsx
@@ -1,16 +1,29 @@
 import React, { Fragment } from 'react';
-import Link from "next/link";
+import Link                from 'next/link';
 
-const AboutPage = ( { result } ) => (
-  <Fragment>
-    <p>
-      Welcome to AboutPage
-    </p>
-
-    <Link href={'/'}>
-      <a>Page Accueil</a>
-    </Link>
-  </Fragment>
+const AboutPage = ({ result }) => (
+  <div>
+    { result.map((datas, i) => (
+      <div key={ i }>
+        { console.log({ datas }) }
+        <div className={ datas.slice_type }>
+          { datas.items.map((item, j) => (
+            <div key={ j }>
+              { item.block_title && item.block_title[ 0 ] &&
+                <h2>{ item.block_title[ 0 ].text }</h2>
+              }
+              { item.sub_title && item.sub_title[ 0 ] &&
+                <h5>{ item.sub_title[ 0 ].text }</h5>
+              }
+              { item.description && item.description[ 0 ] &&
+                <p>{ item.description[ 0 ].text }</p>
+              }
+            </div>
+          )) }
+        </div>
+      </div>
+    )) }
+  </div>
 );
 
 export default AboutPage;
--- pages/about.js
+++ pages/about.js
@@ -1,33 +1,30 @@
 import React, { Fragment } from 'react';
+import Head                from 'next/head';
+import Prismic             from 'prismic-javascript';
+
 import '../styles/style.scss';
-import Head from 'next/head';
 
-import AboutPage from '../Components/Pages/AboutPage';
+import { PRISMIC_API } from '../config';
+import AboutPage       from '../Components/Pages/AboutPage';
+import MainComponent   from '../Components/Main/Main';
 
-import Prismic from 'prismic-javascript';
-import { PRISMIC_API} from "../config";
-import MainComponent from "../Components/Main/Main";
-
-const About = ({result}) => (
+const About = ({ body }) => (
   <Fragment>
     <Head>
       <title>Lou Carter Gallery - about</title>
     </Head>
-
+    
     <MainComponent>
-      <AboutPage result={result} />
+      <AboutPage result={ body }/>
     </MainComponent>
   </Fragment>
 );
 
-About.getInitialProps = async ({}) => {
-  const API = await Prismic.api( PRISMIC_API );
-
-  const result = await API.query(
-    Prismic.Predicates.at( 'document.type', 'about' ), { lang: 'fr-FR'}
-  );
-
-  return { result: result.results[0] }
+About.getInitialProps = async({}) => {
+  const API = await Prismic.api(PRISMIC_API);
+  const response = await API.query(Prismic.Predicates.at('document.type', 'about'), { lang: 'fr-FR' });
+  const { body } = response.results[ 0 ].data;
+  return { body };
 };
 
 export default About;
