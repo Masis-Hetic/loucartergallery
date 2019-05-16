caf6693a83214e142c78b9e7829844db8358e739
masisg <masis.gulmez@hetic.net>
2019-05-16 18:05:37 +0200

    Add link on carousel

--- Components/Pages/HomePage.jsx
+++ Components/Pages/HomePage.jsx
@@ -70,7 +70,7 @@ class HomePage extends Component {
             <Slider { ...settings }>
             { carousel.map( ( img, i ) => (
               <div key={ i } style={{ position: 'relative' }}>
-                <div onClick={() => Router.push('/about')}>
+                <div onTouchMoveCapture={() => Router.push('/about')} onClick={() => Router.push('/about')}>
                   <img
                     src={ img.src }
                     alt=""
