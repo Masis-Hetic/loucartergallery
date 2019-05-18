20760b0004e359d1273f869c91d0d32da8d13b61
masisg <masis.gulmez@hetic.net>
2019-05-18 12:52:49 +0200

    easy-swipe test

--- Components/Pages/HomePage.jsx
+++ Components/Pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-import Slider from "react-slick";
+import Swipe from 'react-easy-swipe';
 
 const carousel = [
   {
@@ -35,29 +35,12 @@ class HomePage extends Component {
     this.setState( { visible: visible - 1 } );
   };
 
+  onSwipeEnd = () => {
+    this.incrementFunction();
+  };
+
   render() {
-// https://github.com/akiran/react-slick/issues/1160
-    const settings = {
-      dots: false,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      arrows: true,
-      fade: true,
-      swipe: false,
-      responsive: [
-        {
-          breakpoint: 700,
-          settings: {
-            arrows: false,
-            autoplay: true,
-            autoplaySpeed: 2500,
-            swipe: true,
-          }
-        }
-      ]
-    };
+    const { visible } = this.state;
 
     return (
       <Fragment>
@@ -65,21 +48,22 @@ class HomePage extends Component {
         <div className="home-image-wrapper">
 
           <div className="image-wrapper">
-            <Slider { ...settings }>
-            { carousel.map( ( img, i ) => (
-              <div
-                key={ i }
-                style={{ position: 'relative' }}
-              >
-                <div>
+            <Swipe
+              onSwipeEnd={this.onSwipeEnd}
+              allowMouseEvents={true}
+            >
+              { carousel.map( ( img, i ) => (
+                <div
+                  className={`swipe-wrapper ${visible === i && 'visible'}`}
+                  key={ i }
+                >
                   <img
                     src={ img.src }
                     alt=""
                   />
                 </div>
-              </div>
-            ) ) }
-            </Slider>
+              ) ) }
+            </Swipe>
           </div>
 
         </div>
@@ -90,3 +74,101 @@ class HomePage extends Component {
 }
 
 export default HomePage;
+
+
+
+
+
+
+// import React, { Component, Fragment } from 'react';
+// import Slider from "react-slick";
+//
+// const carousel = [
+//   {
+//     src: '../../static/images/homepage-white.jpg',
+//     alt: ''
+//   },
+//   {
+//     src: '../../static/images/homepage-lou.jpg',
+//     alt: ''
+//   }
+// ];
+//
+// class HomePage extends Component {
+//   state = {
+//     visible: 0
+//   };
+//
+//   incrementFunction = () => {
+//     const { visible } = this.state;
+//     if (visible >= carousel.length - 1) {
+//       this.setState( { visible: 0 } );
+//       return;
+//     }
+//     this.setState( { visible: visible + 1 } );
+//   };
+//
+//   decrementFunction = () => {
+//     const { visible } = this.state;
+//     if (visible <= 0) {
+//       this.setState( { visible: carousel.length - 1 } );
+//       return;
+//     }
+//     this.setState( { visible: visible - 1 } );
+//   };
+//
+//   render() {
+// // https://github.com/akiran/react-slick/issues/1160
+//     const settings = {
+//       dots: false,
+//       infinite: true,
+//       speed: 500,
+//       slidesToShow: 1,
+//       slidesToScroll: 1,
+//       arrows: true,
+//       fade: true,
+//       swipe: false,
+//       responsive: [
+//         {
+//           breakpoint: 700,
+//           settings: {
+//             arrows: false,
+//             autoplay: true,
+//             autoplaySpeed: 2500,
+//             swipe: true,
+//           }
+//         }
+//       ]
+//     };
+//
+//     return (
+//       <Fragment>
+//
+//         <div className="home-image-wrapper">
+//
+//           <div className="image-wrapper">
+//             <Slider { ...settings }>
+//             { carousel.map( ( img, i ) => (
+//               <div
+//                 key={ i }
+//                 style={{ position: 'relative' }}
+//               >
+//                 <div>
+//                   <img
+//                     src={ img.src }
+//                     alt=""
+//                   />
+//                 </div>
+//               </div>
+//             ) ) }
+//             </Slider>
+//           </div>
+//
+//         </div>
+//
+//       </Fragment>
+//     );
+//   }
+// }
+//
+// export default HomePage;
