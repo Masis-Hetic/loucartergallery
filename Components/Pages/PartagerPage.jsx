517430133e957045a6d80ca7a39286954a736076
CedricSalaun <salaun.cedric@gmail.com>
2019-05-26 20:41:38 +0200

    wip

---
+++ Components/Pages/PartagerPage.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const FounderPage = ({ result }) => (
+  <div className="founder-container text-gradiant">
+    { result.map((datas, i) => (
+      <div key={ i } className="founder-block">
+        <div className={ `${ datas.slice_type }` }>
+          { datas.items.map((item, j) => (
+            <div key={ j } className="founder-item-block">
+              { item.block_title && item.block_title[ 0 ] && <h2>{ item.block_title[ 0 ].text }</h2> }
+              { item.sub_title && item.sub_title[ 0 ] && <h3>{ item.sub_title[ 0 ].text }</h3> }
+              { item.description && item.description[ 0 ] && <p>{ item.description[ 0 ].text }</p> }
+            </div>
+          )) }
+        </div>
+      </div>
+    )) }
+  </div>
+);
+
+export default FounderPage;
