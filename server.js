c6a18ef20c2b1acc1b1382565041ca54dc3b654e
masisg <masis.gulmez@hetic.net>
2019-05-26 11:05:21 +0200

    test dynamic routes

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
+const express = require('express');
 const next = require('next');
-const routes = require('./routes');
 
+const port = parseInt(process.env.PORT, 10) || 3000;
 const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dir: '.', dev });
-// const handler = routes.getRequestHandler(app);
-const handler = routes.getRequestHandler( app, ( { req, res, route, query } ) => {
-  console.log('s', query);
-  console.log('s', req);
-  return app.render( req, res, route.page, query );
-} );
-
-const { createServer } = require('http');
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
-  createServer(handler).listen(3000);
+  const server = express();
+
+  server.get('/campagnes/:slug', (req, res) => {
+    return app.render(req, res, '/campagnes', { slug: req.params.slug })
+  });
+
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  });
+
+  server.listen(port, err => {
+    if (err) throw err;
+    console.log(`> Ready on http://localhost:${port}`)
+  })
 });
+
+
+// const next = require('next');
+// const routes = require('./routes');
+//
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dir: '.', dev });
+// // const handler = routes.getRequestHandler(app);
+// const handler = routes.getRequestHandler( app, ( { req, res, route, query } ) => {
+//   console.log('s', query);
+//   console.log('s', req);
+//   return app.render( req, res, route.page, query );
+// } );
+//
+// const { createServer } = require('http');
+//
+// app.prepare().then(() => {
+//   createServer(handler).listen(3000);
+// });
