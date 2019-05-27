c48998042e6d720633359b7f63a0ef2ebf284e1d
CedricSalaun <salaun.cedric@gmail.com>
2019-05-27 22:58:44 +0200

    null zero

--- Components/Pages/PartagerPage.jsx
+++ Components/Pages/PartagerPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const FounderPage = ({ result }) => (
-  <div className="founder-container text-gradiant">
+const PartagerPage = ({ result }) => (
+  <div className="partager-container text-gradiant">
     { result.map((datas, i) => (
-      <div key={ i } className="founder-block">
+      <div key={ i } className="partager-block">
         <div className={ `${ datas.slice_type }` }>
           { datas.items.map((item, j) => (
-            <div key={ j } className="founder-item-block">
+            <div key={ j } className="partager-item-block">
               { item.block_title && item.block_title[ 0 ] && <h2>{ item.block_title[ 0 ].text }</h2> }
               { item.sub_title && item.sub_title[ 0 ] && <h3>{ item.sub_title[ 0 ].text }</h3> }
               { item.description && item.description[ 0 ] && <p>{ item.description[ 0 ].text }</p> }
@@ -18,4 +18,4 @@ const FounderPage = ({ result }) => (
   </div>
 );
 
-export default FounderPage;
+export default PartagerPage;
