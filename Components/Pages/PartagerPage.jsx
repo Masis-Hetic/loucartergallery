8538f309ea73a33d1a4fea7da1d140ee7c44d537
CedricSalaun <salaun.cedric@gmail.com>
2019-05-28 23:27:41 +0200

    add soon

--- Components/Pages/PartagerPage.jsx
+++ Components/Pages/PartagerPage.jsx
@@ -1,20 +1,10 @@
 import React from 'react';
 
-const PartagerPage = ({ result }) => (
+const PartagerPage = ({ body }) => (
   <div className="partager-container">
-    { result.map((datas, i) => (
-      <div key={ i } className="partager-block">
-        <div className={ `${ datas.slice_type }` }>
-          { datas.items.map((item, j) => (
-            <div key={ j } className="partager-item-block">
-              { item.block_title && item.block_title[ 0 ] && <h2>{ item.block_title[ 0 ].text }</h2> }
-              { item.sub_title && item.sub_title[ 0 ] && <h3>{ item.sub_title[ 0 ].text }</h3> }
-              { item.description && item.description[ 0 ] && <p>{ item.description[ 0 ].text }</p> }
-            </div>
-          )) }
-        </div>
-      </div>
-    )) }
+    {/*{ body.map((datas, i) => (*/}
+      { console.log({ body }) }
+    {/*)) }*/}
   </div>
 );
 
