446270bc7adb599ad4422a6a0312f21573f9b18e
masisg <masis.gulmez@hetic.net>
2019-05-30 12:53:12 +0200

    gallery carousel mobile

--- Components/Pages/Campaign.jsx
+++ Components/Pages/Campaign.jsx
@@ -7,7 +7,7 @@ class Campaign extends Component {
     isOpen: false
   };
 
-  openSlider = () => this.setState({ isOpen: !this.state.isOpen });
+  openSlider = () => this.setState( { isOpen: !this.state.isOpen } );
 
   render() {
     const { campaign } = this.props;
@@ -17,13 +17,13 @@ class Campaign extends Component {
     return (
       <div className="wrapper">
 
-        <div className={`slide-mobile ${isOpen ? 'open' : 'close'}`}>
-          <p onClick={this.openSlider}>
-            <svg style={{width: 28, height: 28}} viewBox="0 0 24 24">
-              <path fill="#fff" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" />
+        <div className={ `slide-mobile ${ isOpen ? 'open' : 'close' }` }>
+          <p onClick={ this.openSlider }>
+            <svg style={ { width: 28, height: 28 } } viewBox="0 0 24 24">
+              <path fill="#fff" d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z"/>
             </svg>
           </p>
-          {console.log(campaign.data.images.length)}
+          { console.log( campaign.data.images.length ) }
           <ul>
             { campaign.data.images.map( ( img, i ) =>
               <li key={ i }>
@@ -48,6 +48,7 @@ class Campaign extends Component {
         </div>
 
         <div className="text-wrapper">
+
           <div className="text">
             <h1>{ campaign.data.title[ 0 ].text }</h1>
 
@@ -56,17 +57,24 @@ class Campaign extends Component {
               { campaign.data.description.map( ( p, i ) => <SanitizedHTML key={ i } html={ p.text }/> ) }
             </div>
 
-            <p>{ campaign.data.fin_de_description[0].text && campaign.data.fin_de_description[0].text }</p>
+            <p>{ campaign.data.fin_de_description[ 0 ].text && campaign.data.fin_de_description[ 0 ].text }</p>
 
             <p>Détail des oeuvres</p>
           </div>
+
         </div>
 
-        <style jsx>{`
+        {/*
+          Calcul de la taille du before (en desktop) :
+          1- Nombre de slides * 60vw (la taille d'une slide)
+          2- On rajoute la marge entre chaque slide : 40vw (sauf la dernière)
+          3- On rajoute les 5 vw qui permettent de centrer la dernière image
+         */ }
+        <style jsx>{ `
         .slides .slider-wrapper ul::before {
-          width: ${campaign.data.images.length * 100}vw;
+          width: ${ ( campaign.data.images.length * 60 ) + ( ( campaign.data.images.length - 1 ) * 40 ) + 5 }vw;
         }
-        `}</style>
+        ` }</style>
       </div>
     );
   }
