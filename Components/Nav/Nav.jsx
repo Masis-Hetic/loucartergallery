ddeb96359bb66da8268921953f0d9adf602e717c
masisg <masis.gulmez@hetic.net>
2019-05-31 11:09:13 +0200

    Newsletter popin

--- Components/Nav/Nav.jsx
+++ Components/Nav/Nav.jsx
@@ -25,6 +25,14 @@ const Nav = ( { nav } ) => {
     openList( id );
   };
 
+  const [ newsletter, toggleModal ] = useState(false);
+  const isNewsletter = param => {
+    if (param.toLowerCase() === 'newsletter') {
+      toggleModal && openMenu(false);
+      toggleModal( !newsletter );
+    }
+  };
+
   // noinspection JSUnresolvedVariable
   return (
     <Fragment>
@@ -65,7 +73,7 @@ const Nav = ( { nav } ) => {
                             )
                             : (
                               <Fragment key={ i }>
-                                <p className="except">{ sublink.primary.link_two[ 0 ].text }</p>
+                                <p className="except" onClick={ () => isNewsletter(sublink.primary.link_two[0].text)}>{ sublink.primary.link_two[ 0 ].text }</p>
                                 <ul className={ `third-step ${ isListOpen ? 'open' : 'close' }` }>
                                   { sublink.items.map( ( thirdLink, i ) =>
                                     <li key={ i } onClick={ toggleMenu }>
@@ -106,18 +114,100 @@ const Nav = ( { nav } ) => {
         </div>
       </header>
 
+      <div className={newsletter ? 'open-newsletter' : 'close-newsletter'}>
+        <div className="newsletter-wrapper">
+          <p>Inscrivez-vous à notre newsletter</p>
+          <div className="close-newsletter-btn" onClick={() => isNewsletter('newsletter')}>X</div>
+          <form>
+            <div className="input-wrapper">
+              <label htmlFor="mail">Adresse e-mail :</label>
+              <input id="mail" type="email" placeholder="monemail@gmail.com" />
+            </div>
+            <input type="submit" value="S'inscrire" />
+          </form>
+        </div>
+      </div>
+
       <style jsx>{`
-        .nav-links ul > li ul {
-          height: 0;
-          opacity: 0;
-          transition: .1s ease-in-out;
-        }
-        
-        .nav-links ul > li:nth-of-type(${ isListOpen && isList !== null && isList + 1 }) ul {
-          height: unset;
-          opacity: 1;
-          transition: .4s ease-in-out;
-        }
+      .close-newsletter {
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        bottom: -100%;
+        opacity: 0;
+        transition: .3s ease-in-out;
+      }
+      .open-newsletter {
+        opacity: 1;
+        position: fixed;
+        width: 100%;
+        height: 100vh;
+        background: rgba(84, 110, 122, .6);
+        transition: .3s ease-in-out;
+      }
+      .newsletter-wrapper {
+        padding: 4rem;
+        background: #080808;
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        width: 40%;
+        height: 50%;
+      }
+      .newsletter-wrapper p {
+        text-align: center;
+        font-size: 1.5rem;
+      }
+      .newsletter-wrapper form {
+        width: 100%;
+        height: calc(100% - 4rem);
+        margin-top: 3rem;
+      }
+      .input-wrapper {
+        margin-bottom: 3rem;
+      }
+      form label {
+        text-align: center;
+        margin-bottom: 2rem;
+      }
+      form input {
+        line-height: 4;
+        padding-left: 1rem;
+        border-radius: 4px;
+        border: none;
+        margin: 0 auto;
+      }
+      .newsletter-wrapper form label, form input {
+        display: block; 
+      }
+      input[type="email"] {
+        width: 50%;
+      }
+      input[type="submit"] {
+        margin-top: 1rem;
+        padding: 0 1rem;
+        display: table;
+      }
+      .close-newsletter-btn {
+        position: absolute;
+        top: 20px;
+        right: 20px;
+        cursor: pointer;
+      }
+      
+      
+      .nav-links ul > li ul {
+        height: 0;
+        opacity: 0;
+        transition: .1s ease-in-out;
+      }
+      
+      .nav-links ul > li:nth-of-type(${ isListOpen && isList !== null && isList + 1 }) ul {
+        height: unset;
+        opacity: 1;
+        transition: .4s ease-in-out;
+      }
       `}</style>
 
     </Fragment>
