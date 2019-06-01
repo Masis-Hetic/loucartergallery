564864bb468314e16e87f2d29eb7477e2e77c4b7
masisg <masis.gulmez@hetic.net>
2019-06-01 08:27:11 +0200

    newsletter _ test

--- helpers/config.js
+++ helpers/config.js
@@ -4,9 +4,9 @@ const dc = MAILCHIMP_API_KEY.split('-')[1];
 
 export const MAILCHIMP_API_URL = `https://${dc}.api.mailchimp.com/3.0/`;
 
-export const LIST_ID = '0'; // TODO récupéré la mail champ list id
+export const LIST_ID = '6a7e155805'; // TODO récupéré la mail champ list id
 
 export const ADDRESS = 'https://loucarter.netlify.com/'; // TODO ajouter la bonne address ou verifier
 
 // export const ADD_MEMBER_URL = `https://${ADDRESS}/${MAILCHIMP_API_URL}lists/${LIST_ID}/members/`;
-export const ADD_MEMBER_URL = `${MAILCHIMP_API_URL}`; // TODO trouver la bonne URL
+export const ADD_MEMBER_URL = `${MAILCHIMP_API_URL}/lists/${LIST_ID}/members/`; // TODO trouver la bonne URL
