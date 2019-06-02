6587fd4f6ae89cb0d5e6593903e47155a3e5124f
masisg <masis.gulmez@hetic.net>
2019-06-03 01:06:01 +0200

    Artistes

--- pages/eshop.js
+++ pages/eshop.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react';
+import React from "react";
+import MainComponent from "../Components/Main/Main";
+import Prismic from "prismic-javascript";
+import { PRISMIC_API } from "../config";
 
-class Eshop extends Component {
-  render() {
-    return (
-      <div>
-        
-      </div>
-    );
-  }
-}
+const Eshop = () => {
+  return (
+    <MainComponent>
+      <img
+        src="../static/images/coming_soon/e_1080X1920.jpg"
+        alt=""
+      />
+      <style jsx>{`
+      img {
+        display: block;
+        width: 100vw;
+        height: 100vh;
+      }
+      `}</style>
+    </MainComponent>
+  )
+};
+
+
+Eshop.getInitialProps = async () => {
+  const API = await Prismic.api( PRISMIC_API );
+
+  const artistes = await API.query(
+    Prismic.Predicates.at( 'document.type', 'eshop' ), { lang: 'fr-FR' }
+  );
+
+  return { artistes: artistes.results[ 0 ] }
+};
 
 export default Eshop;
