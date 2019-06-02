eeea5b58c854826985aa89d936e3bba4e64cf541
masisg <masis.gulmez@hetic.net>
2019-06-02 21:52:39 +0200

    newsletter _ test

--- helpers/config.js
+++ helpers/config.js
@@ -5,11 +5,7 @@ const dc = MAILCHIMP_API_KEY.split('-')[1];
 export const MAILCHIMP_API_URL = `https://${dc}.api.mailchimp.com/3.0`;
 
 export const LIST_ID = '6a7e155805';
-// https://medium.com/@dddotcom/intro-to-mailchimp-api-3-0-adding-members-to-a-mailing-list-and-testing-with-postman-f056d900e0d9
 
 export const ADDRESS = 'https://loucarter.netlify.com/'; // TODO ajouter la bonne address ou verifier
 
-// export const ADD_MEMBER_URL = `https://${ADDRESS}/${MAILCHIMP_API_URL}lists/${LIST_ID}/members/`;
-
-// export const ADD_MEMBER_URL = `${MAILCHIMP_API_URL}/lists/${LIST_ID}/members/`; // TODO trouver la bonne URL
-export const ADD_MEMBER_URL = `https://cors-anywhere.herokuapp.com/${MAILCHIMP_API_URL}/lists/${LIST_ID}/members/`; // TODO trouver la bonne URL
+export const ADD_MEMBER_URL = `https://cors-anywhere.herokuapp.com/${MAILCHIMP_API_URL}/lists/${LIST_ID}/members/`;
--- helpers/mailchimp.js
+++ helpers/mailchimp.js
@@ -1,40 +1,8 @@
 import axios from 'axios';
 
-import { ADD_MEMBER_URL, MAILCHIMP_API_KEY, MAILCHIMP_API_URL } from './config';
+import { ADD_MEMBER_URL, MAILCHIMP_API_KEY } from './config';
 
-// export const subscribeToNews = (req, res, next) => {
-//   console.log({req});
-//
-//   const email = req.body.email; // Email entered
-//   const dataCenter = 'us1';
-//   const apiKey = 'c03e070b4411f258d90f5b7e834aecf8-us20';
-//   const listID = 'LIST_ID';
-//
-//   const options = {
-//     url    : ``,
-//     method : 'POST',
-//     headers: { 'content-type': 'application/json', 'Authorization': `apikey ${ apiKey }` },
-//     body   : JSON.stringify({ email_address: email, status: 'subscribed' }),
-//   };
-//
-//   request(options,  (error, response, body) => {
-//     try {
-//       let respObj = {};
-//       if (response.statusCode === 200) {
-//         respObj = { success: `Subscribed using ${email}`, message: JSON.parse(response.body) };
-//       } else {
-//         respObj = { error: `Error trying to subscribe ${email}. Plese try again.`, message: JSON.parse(response.body) }
-//       }
-//       res.send(respObj);
-//     } catch (e) {
-//       const respErrorObj = { error: `There was an error with your request`, message: error.message };
-//       res.send(respErrorObj);
-//     }
-//   });
-//   next();
-// };
-
-export const subscribeToNews = ( email_address ) => { // TODO valider la request AXIOS
+export const subscribeToNews = ( email_address ) => {
   axios.post(
     ADD_MEMBER_URL,
     {
