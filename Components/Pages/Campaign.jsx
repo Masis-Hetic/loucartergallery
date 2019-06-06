3123d9c439076a883a62f5f8403777f36203cc31
masisg <masis.gulmez@hetic.net>
2019-06-06 11:45:57 +0200

    Images on campaigns

--- Components/Pages/Campaign.jsx
+++ Components/Pages/Campaign.jsx
@@ -6,6 +6,15 @@ class Campaign extends Component {
     isOpen: false
   };
 
+  componentDidMount() {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    document.querySelector('body').style.height = 'calc(var(--vh, 1vh) * 100)';
+    document.body.style.height = '100vh';
+  }
+
   openSlider = () => this.setState( { isOpen: !this.state.isOpen } );
 
   render() {
