6ba0a38a16dea5b83f6ed4c220da499da74c39f7
masisg <masis.gulmez@hetic.net>
2019-06-08 10:40:23 +0200

    .

--- helpers/functions.js
+++ helpers/functions.js
@@ -1,6 +1,6 @@
 export const sliceUrl = url => {
   if (!url) return false;
-  return url.slice( 0, 6 ) === 'https:' ? url.substring( 6 ) : url.slice( 0, 5 ) === 'http:' ? url.substring( 5 ) : url;
+  return url.slice( 0, 7 ) === 'https:' ? url.substring( 7 ) : url.slice( 0, 6 ) === 'http:' ? url.substring( 6 ) : url;
 };
 
 export const validateEmail = (email) => {
