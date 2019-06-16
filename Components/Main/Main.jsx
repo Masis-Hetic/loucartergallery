01f83cd6eb93d743c99509e2e27bb7492d89874a
masisg <masis.gulmez@hetic.net>
2019-06-16 21:48:35 +0200

    poulet

--- Components/Main/Main.jsx
+++ Components/Main/Main.jsx
@@ -7,21 +7,21 @@ import Cookies from "../Cookies/Cookies";
 const MainComponent = props => (
   <main className="clearfix">
 
+    <div className="logo-wrapper">
+      <Link href={ '/' }>
+        <a className="logo">
+          <img src="../../static/icons/loucarter_logo.png" alt=""/>
+        </a>
+      </Link>
+    </div>
     <div className="head-wrapper">
-      <div className="logo-wrapper">
-        <Link href={'/'}>
-          <a className="logo">
-            <img src="../../static/icons/loucarter_logo.png" alt="" />
-          </a>
-        </Link>
-      </div>
 
     </div>
-    <Nav />
+    <Nav/>
 
     { props.children }
 
-    <Cookies />
+    <Cookies/>
   </main>
 );
 
