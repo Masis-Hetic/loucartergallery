1c99fd5d2d317af18cf7d960ce296e750a9fe45f
masisg <masis.gulmez@hetic.net>
2019-08-20 12:36:24 +0200

    Gallery

--- Components/Pages/Gallery.style.js
+++ Components/Pages/Gallery.style.js
@@ -9,6 +9,13 @@ const Gallery = styled.div`
   position: absolute;
   bottom: 0;
   overflow-y: hidden;
+  
+  ${media.mobile`
+    height: calc(100% - 20%);
+    grid-template-columns: unset;
+    display: block;
+    overflow-y: unset;
+  `}
 `;
 
 Gallery.WrapperOne = styled.div`
@@ -16,6 +23,14 @@ Gallery.WrapperOne = styled.div`
   grid-column-start: 1;
   grid-column-end: 2;
   padding: 0 20%;
+  
+  ${media.mobile`
+    height: unset;
+    grid-column-start: unset;
+    grid-column-end: unset;
+    padding: 0 7% 0 5%;
+    margin-bottom: 50px;
+  `}
 `;
 
 Gallery.WrapperTwo = styled.div`
@@ -28,26 +43,49 @@ Gallery.WrapperTwo = styled.div`
   overflow: -moz-scrollbars-none;
   -ms-overflow-style : none;
   scrollbar-width    : none;
-  &::-webkit-scrollbar { width : 0 !important }
+  &::-webkit-scrollbar { width : 0 !important; }
   
-  ${media.smallDesktop`
-    
-  `}
+  @media (min-width: 4000px) {
+    font-size: 2rem;
+  }
   
   @media (min-width: 2560px) {
     font-size: 1.2rem;
     padding: 0 10% 5%;
   }
   
-  @media (min-width: 4000px) {
-    font-size: 2rem;
-  }
+  ${media.smallDesktop`
+    
+  `}
+  
+  ${media.mobile`
+    height: unset;
+    grid-column-start: unset;
+    grid-column-end: unset;
+    overflow-y: unset;
+    overflow: unset;
+    
+    -ms-overflow-style : unset;
+    scrollbar-width    : unset;
+    &::-webkit-scrollbar { width : unset !important; }
+    
+    padding: 0 5%;
+    margin-bottom: 50px;
+  `}
+  
 `;
 
 Gallery.WrapperThree = styled.div`
   height: 100%;
   grid-column-start: 3;
   padding: 0 20%;
+  
+  ${media.mobile`
+    grid-column-start: unset;
+    height: unset;
+    padding: 0 5%;
+    margin-bottom: 80px;
+  `}
 `;
 
 Gallery.BlockTitle = styled.h2`
@@ -62,18 +100,27 @@ Gallery.BlockTitle = styled.h2`
   @media (min-width: 4000px) {
     font-size: 3rem;
   }
+  
+  ${media.mobile`
+    font-size: 1.2rem;
+  `}
 `;
 
 Gallery.SubTitle = styled.p`
   text-align: center;
   
+  @media (min-width: 4000px) {
+    font-size: 2rem;
+  }
+  
   @media (min-width: 2560px) {
     font-size: 1.2rem;
   }
   
-  @media (min-width: 4000px) {
-    font-size: 2rem;
-  }
+  ${media.mobile`
+    line-height: 2.7 !important;
+    font-size: 1rem;
+  `}
 `;
 
 export default Gallery;
