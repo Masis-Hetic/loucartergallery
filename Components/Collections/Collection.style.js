4917b24a3e843207a48e8e4131473f5dd8f89861
Masis <masis.gulmez@hetic.net>
2019-08-27 13:39:00 +0200

    Collection wip

--- Components/Collections/Collection.style.js
+++ Components/Collections/Collection.style.js
@@ -1,11 +1,15 @@
 import styled from "styled-components";
+import media from "../../helpers/media"
 import COLORS from "../../helpers/colors";
 
 const SingleCollection = styled.div`
   width: 100vw;
   height: 100vh;
   overflow-y: hidden;
-  // position: fixed;
+  
+  ${media.mobile`
+		overflow-y: unset;    
+  `}
 `;
 
 SingleCollection.Ul = styled.ul`
@@ -16,6 +20,13 @@ SingleCollection.Ul = styled.ul`
   top: 50%;
   left: 50%;
   transform: translateX(-50%);
+  
+  ${media.mobile`
+    top: 10rem;
+    width: 90vw;
+    flex-direction: column;
+    margin-bottom: 10rem;
+  `}
 `;
 
 SingleCollection.Li = styled.li`
@@ -24,10 +35,89 @@ SingleCollection.Li = styled.li`
   flex: 1 0 45%;
   cursor: pointer;
   margin: ${ props => props.margin };
+  
+  ${media.mobile`
+    margin: 0 0 40px 0;
+  `}
 `;
 
 SingleCollection.Img = styled.img`
+	display: block;
+	width: 100%;
+	height: 100%;
+`;
 
+SingleCollection.SelectedImage = styled.div`
+	width: calc(100% - ${ props => props.left }px);
+	left: ${ props => props.left }px;
+	height: calc(${ props => (props.height * 2) + 44 }px);
+	position: fixed;
+	top: 50%;
+	transform: translateY(-50%);
+	z-index: 2;
+	display: ${ props => props.display };
+	
+	${media.mobile`
+    width: 100%;
+    height: 100vh;
+    left: unset;
+    top: unset;
+    transform: unset;
+    bottom: 0;
+    background: #080808;
+    overflow-y: scroll;
+    flex-direction: column;
+    z-index: 100;
+	`}
+`;
+
+SingleCollection.BigImage = styled.img`
+	display: block;
+	width: calc(${ props => props.width }px + 2px);
+	height: 100%;
+	border: 1px solid ${ COLORS.white };
+	
+	${media.mobile`
+		width: 100%;
+		margin: 0 auto;
+		object-fit: contain;
+	`}
+`;
+
+SingleCollection.DescriptionWrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	padding: 0 50px;
+	background: #080808;
+	
+	${media.mobile`
+		margin: 50px 0;
+	`}
+`;
+
+SingleCollection.ArtistName = styled.p`
+	font-size: 1.3rem;
+`;
+
+SingleCollection.CollectionName = styled.p`
+	margin: 30px 0 5px 0;
+`;
+
+SingleCollection.ArtName = styled.p`
+	margin-bottom: 10px;
+`;
+
+SingleCollection.CloseBtn = styled.div`
+	position: absolute;
+	top: 0;
+	right: 50px;
+	cursor: pointer;
+	
+	${media.mobile`
+		top: 30px;
+		right: 30px;
+	`}
 `;
 
 export default SingleCollection;
--- pages/collections/[collection].js
+++ pages/collections/[collection].js
@@ -4,17 +4,19 @@ import Prismic             from 'prismic-javascript';
 import getConfig           from 'next/config';
 import MainComponent       from "../../Components/Main/Main";
 import Head                from "next/head";
+import CloseBtn from "../../static/icons/close-btn";
 
 const { publicRuntimeConfig } = getConfig();
 
 /**
  * @property { string } dimensions
+ * @property { string } artist_name
  * @param query
  * @param collection
  * @returns {*}
  * @constructor
  */
-const Collection = ( { query, collection } ) => {
+const Collection = ( { collection } ) => {
   const ul = useRef(null);
 
   const [ index, setIndex ] = useState(0);
@@ -26,7 +28,6 @@ const Collection = ( { query, collection } ) => {
   const handleClick = (e, i) => {
     setIndex(i);
     setHeight(e.target.offsetHeight);
-    console.log(ul.current.getBoundingClientRect());
     setLeft(ul.current.getBoundingClientRect().left);
     setWidth(ul.current.getBoundingClientRect().width);
     setDisplay(!display);
@@ -39,46 +40,36 @@ const Collection = ( { query, collection } ) => {
       </Head>
       <MainComponent>
 
-        <SingleCollection /* onWheel={e => handleScroll(e)} */>
+        <SingleCollection>
           <SingleCollection.Ul ref={ ul }>
             {collection.map((art, i)=>
-              <SingleCollection.Li onClick={ e => handleClick(e, i) } margin={ i % 2 === 0 ? '0 40px 40px 0' : '0 0 40px 0' } key={i}>
-                <img style={{ display: 'block', width: '100%', height: '100%' }} src={ art.data.image.url } alt=""/>
+              <SingleCollection.Li
+                onClick={ e => handleClick(e, i) }
+                margin={ i % 2 === 0 ? '0 40px 40px 0' : '0 0 40px 0' } key={i}
+              >
+                <SingleCollection.Img src={ art.data.image.url } alt=""/>
               </SingleCollection.Li>
             )}
           </SingleCollection.Ul>
-          <div
-            style={{
-              width: `calc(100% - ${left}px)`,
-              left: `${left}px`,
-              height: `calc(${ ( (height * 2) + 44 ) }px)`,
-              position: 'fixed',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              zIndex: 100,
-              display: `${ !display ? 'none' : 'flex' }`,
-            }}
+
+          <SingleCollection.SelectedImage
+            left={ left }
+            height={ height }
+            display={ !display ? 'none' : 'flex' }
             onClick={() => setDisplay(false)}
           >
-            {console.log(collection[index].data)}
-            <img src={ collection[index].data.image.url } alt="" style={{display: 'block', width: `calc(${width}px + 2px)`, height: '100%'}}/>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center'
-              }}
-            >
-              <p>Ajouter nom de l'artiste ici</p>
-              <p dangerouslySetInnerHTML={{ __html: collection[index].data.collection_name[0].text }}/>
-              <p dangerouslySetInnerHTML={{ __html: collection[index].data.name[0].text }}/>
+            <SingleCollection.BigImage src={ collection[index].data.image.url } alt="" width={ width } />
+            <SingleCollection.DescriptionWrapper>
+              <SingleCollection.ArtistName>{collection[index].data.artist_name[0].text}</SingleCollection.ArtistName>
+              <SingleCollection.CollectionName dangerouslySetInnerHTML={{ __html: collection[index].data.collection_name[0].text }}/>
+              <SingleCollection.ArtName dangerouslySetInnerHTML={{ __html: collection[index].data.name[0].text }}/>
               <p dangerouslySetInnerHTML={{ __html: collection[index].data.dimensions[0].text }}/>
-            </div>
-            <div style={{ position: 'absolute', top: 0, right: 0 }}>X</div>
-          </div>
+            </SingleCollection.DescriptionWrapper>
+            <SingleCollection.CloseBtn>
+              <CloseBtn/>
+            </SingleCollection.CloseBtn>
+          </SingleCollection.SelectedImage>
         </SingleCollection>
-
-
 
       </MainComponent>
     </Fragment>
