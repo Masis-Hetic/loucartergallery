3b8665df67d63c2a1b246cd4174c920d5a23986c
masisg <masis.gulmez@hetic.net>
2019-09-06 21:49:46 +0200

    Body height

--- pages/index.js
+++ pages/index.js
@@ -13,18 +13,7 @@ const { publicRuntimeConfig } = getConfig();
 // noinspection JSUnresolvedVariable
 const Index = ({ result, imgs }) => {
   useEffect(() => {
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerHeight * 0.01;
-    {window &&
-      // Then we set the value in the --vh custom property to the root of the document
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-      window.addEventListener('resize', () => {
-        // We execute the same script as before
-        let vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty('--vh', `${vh}px`);
-      });
-    }
+    document.body.height = window && window.innerHeight;
   });
 
   return (
